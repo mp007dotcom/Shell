af46baa31d3aef3e53f2acfd9f89666d1af4a1ce
ABC <1312mrp@gmail.com>
2026-03-16 12:00:11 +0530

    changes

--- 01-Hello word.sh
+++ 01-Hello word.sh
@@ -3,4 +3,4 @@
 # This is the comment, this will not be executed by bash. This is only for information purpose
 echo "Hello World!"
 
-echo "Hi, I am learning Git"
+echo "Hi, I am learning something"
